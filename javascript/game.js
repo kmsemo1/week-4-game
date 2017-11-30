2a7b596628c6533ff66e95185cdf254f42f69e85
kmsemo1 <32209919+kmsemo1@users.noreply.github.com>
2017-11-29 19:03:02 -0800

    fixed button 1 display

--- javascript/game.js
+++ javascript/game.js
@@ -1,48 +1,54 @@
 // VARIABLES
-// User total
-var total = [];
-console.log(total);
+
+
 // add total array
 // display total
-$("#result").html(total);
+var total = 0;
 
 
 // generate a number for #button-1
-var btnOne = Math.floor((Math.random() * 12) +1);
+var btnOne = Math.floor((Math.random() * 12) + 1);
 console.log(btnOne);
 
 //====================================================================================================
 // START OF GAME GENERATE A NUMBER FOR COMPUTER'S PICK 
+$(document).ready(function () {
+    //generate #computer-pick number
+    var compNumber = Math.floor((Math.random() * 120) + 19);
+    console.log(compNumber);
+    //display #computer-pick number 
+    $("#computer-pick").html(compNumber);
 
-//generate #computer-pick number
-var compNumber = Math.floor((Math.random() * 120) + 19);
-console.log(compNumber);
-//display #computer-pick number 
-$("#computer-pick").html(compNumber);
+    //====================================================================================================
+    // START OF GAME GENERATE A NUMBER FOR ALL 4 CRYSTALS
 
-//====================================================================================================
-// START OF GAME GENERATE A NUMBER FOR ALL 4 CRYSTALS
 
-$(document).(ready function() {
-// when #button-1 is clicked
-$("#button-1").on(function(){
-    var oneTotal = Math.floor(btnOne);
+    // when #button-1 is clicked
+    $("#button-1").on("click", function () {
+        // add the generated number to #result
+        var result = btnOne + total;
+        total += result
+        // User total
+        
+        //display total
+        $("#result").html(result)
+        console.log(total);
 
+    });
+
+
+    // generate a number for #button-2
+    // when #button-2 is clicked
+    // add the generated number to #result
+
+    // generate a number for #button-3
+    // when #button-3 is clicked
+    // add the generated number to #result
+
+    // generate a number for #button-4
+    // when #button-4 is clicked
+    // add the generated number to #result
 });
-// add the generated number to #result
-
-// generate a number for #button-2
-// when #button-2 is clicked
-// add the generated number to #result
-
-// generate a number for #button-3
-// when #button-3 is clicked
-// add the generated number to #result
-
-// generate a number for #button-4
-// when #button-4 is clicked
-// add the generated number to #result
-
 //====================================================================================================
 //END OF GAME
 
@@ -58,3 +64,5 @@ $("#button-1").on(function(){
 // if #result > #computer-pick
 // add +1 to total loss
 // display "you lose" message
+
+//reset using .empty()
