11c0d248a3c30758772c687272a69b154c5c962d
kmsemo1 <32209919+kmsemo1@users.noreply.github.com>
2018-03-15 20:52:43 -0700

    fixed javascript file

--- javascript/game.js
+++ javascript/game.js
@@ -1,94 +1,144 @@
-//====================================================================================================
-// START OF GAME GENERATE A NUMBER FOR COMPUTER'S PICK 
-$(document).ready(function () {
-    // VARIABLES
-    // user total
-    var total = 0;
-    // win total
-    var win = 0;
-    // loss total
-    var loss = 0;
+// GLOBAL VARIABLES
+// =================================================================
 
-    //generate #computer-pick number range of 19-120
-    var compNumber = Math.floor((Math.random() * 111) + 19);
-
-    //display #computer-pick number 
-    $("#computer-pick").html(compNumber);
-
-    // generate a random number for #button-1 range of 1-12 
-    var btnOne = Math.floor((Math.random() * 12) + 1);
-
-    // generate a random number for #button-2 range of 1-12
-    var btnTwo = Math.floor((Math.random() * 12) + 1);
-
-    // generate a random number for #button-3 range of 1-12
-    var btnThree = Math.floor((Math.random() * 12) + 1);
-
-    // generate a random number for #button-4 range of 1-12
-    var btnFour = Math.floor((Math.random() * 12) + 1);
-
-    console.log(btnOne, btnTwo, btnThree, btnFour);
-    console.log(compNumber);
-
-    //====================================================================================================
-    // START OF GAME GENERATE A NUMBER FOR ALL 4 CRYSTALS
-
-
-    // when #button-1 is clicked
-    $("#button-1").on("click", function () {
-        // add increment result of btnOne into total
-        // add the generated number to #result
-        total += btnOne;
-        //display total
-        $("#result").html(total);
-    });
-
-    // when #button-2 is clicked
-    $("#button-2").on("click", function () {
-        // add the generated number to #result
-        total += btnTwo;
-        //display total
-        $("#result").html(total);
-    });
-
-    // when #button-3 is clicked
-    $("#button-3").on("click", function () {
-        // add the generated number to #result
-        total += btnThree;
-        //display total
-        $("#result").html(total);
-    });
-
-    
-    // when #button-4 is clicked
-    $("#button-4").on("click", function () {
-        // add the generated number to #result
-        total += btnFour;
-        //display total
-        $("#result").html(total);
-    });
-
-    
-    // keeping count for win and loss
-    if (total == compNumber) {
-        // add +1 to total wins
-        win++;
-        // display number of wins 
-        $("#Win").html("Wins: " + win);
+// Crystal Variables
+var crystal = {
+    white:
+    {
+      name: "White",
+      value: 0
+    },
+    green:
+    {
+      name: "Green",
+      value: 0
+    },
+    red:
+    {
+      name: "Red",
+      value: 0
+    },
+    blue:
+    {
+      name: "Blue",
+      value: 0
     }
-    if (total > compNumber) {
-        // add +1 to total loss
-        loss++;
-        // display number of  losses
-        $("#Loss").html("Loss: " + loss);
+  };
+  
+  // Scores (Current and Target)
+  var currentScore = 0;
+  var targetScore = 0;
+  
+  // Wins and Losses
+  var winCount = 0;
+  var lossCount = 0;
+  
+  
+  // FUNCTIONS
+  // =================================================================
+  
+  // Helper Function for getting random numbers
+  var getRandom = function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+  
+  // Starts the Game (and restarts the game)
+  var startGame = function() {
+  
+    // Reset the Current Score
+    currentScore = 0;
+  
+    // Set a new Target Score (between 19 and 120)
+    targetScore = getRandom(19, 120);
+  
+    // Set different values for each of the crystals (between 1 and 12)
+    crystal.white.value = getRandom(1, 12);
+    crystal.red.value = getRandom(1, 12);
+    crystal.green.value = getRandom(1, 12);
+    crystal.blue.value = getRandom(1, 12);
+  
+    // Change the HTML to reflect all of these changes
+    $("#your-score").text(currentScore);
+    $("#target-score").text(targetScore);
+  
+  
+    // Testing Console
+    console.log("-----------------------------------");
+    console.log("Target Score: " + targetScore);
+    console.log("White: " + crystal.white.value + " | Green: " + crystal.green.value + " | Red: " + crystal.red.value +
+      " | Blue: " + crystal.blue.value);
+    console.log("-----------------------------------");
+  };
+  
+  // Check if User Won or Lost and Reset the Game
+  var checkWin = function() {
+  
+    // Check if currentScore is larger than targetScore
+    if (currentScore > targetScore) {
+      alert("Sorry. You lost!");
+      console.log("You Lost");
+  
+      // Add to Loss Counter
+      lossCount++;
+  
+      // Change Loss Count HTML
+      $("#loss-count").text(lossCount);
+  
+      // Restart the game
+      startGame();
     }
-
-});
-
-
-//====================================================================================================
-//END OF GAME
-
-// send alert with "You Won"
-// store number of wins
-//reset using .empty() for all crystal values, total value, and compNumber value
+  
+    else if (currentScore === targetScore) {
+      alert("Congratulations! You Won!");
+      console.log("You Won!");
+  
+      // Add to the Win Counter
+      winCount++;
+  
+      // Change Win Count HTML
+      $("#win-count").text(winCount);
+  
+      // Restart the game
+      startGame();
+    }
+  
+  };
+  
+  // Respond to clicks on the crystals
+  var addValues = function(clickedCrystal) {
+  
+    // Change currentScore
+    currentScore += clickedCrystal.value;
+  
+    // Change the HTML to reflect changes in currentScore
+    $("#your-score").text(currentScore);
+  
+    // Call the checkWin Function
+    checkWin();
+  
+    // Testing Console
+    console.log("Your Score: " + currentScore);
+  };
+  
+  // MAIN PROCESS
+  // =================================================================
+  
+  // Starts the Game the First Time.
+  startGame();
+  
+  $("#white").click(function() {
+    addValues(crystal.blue);
+  });
+  
+  $("#red").click(function() {
+    addValues(crystal.red);
+  });
+  
+  $("#green").click(function() {
+    addValues(crystal.green);
+  });
+  
+  $("#blue").click(function() {
+    addValues(crystal.yellow);
+  });
+  
